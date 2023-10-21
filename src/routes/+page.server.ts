f7eb615c103b772931755dc216973b442eda2e01
Ryan Price <rsprice90@gmail.com>
2023-10-21 02:01:22 -0700

    feat: flesh out basic functionality

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,10 +1,56 @@
+import dotenv from 'dotenv';
 import type { Actions } from './$types';
+import { fail } from '@sveltejs/kit';
+import { OpenAI } from 'openai';
+
+dotenv.config();
+
+const apiKey = process.env.OPENAI_API_KEY;
+
+if (!apiKey) {
+    throw new Error("You must provide an API key.");
+}
+
+const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
 export const actions = {
     default: async (event) => {
         console.log({ event });
         const data = event.request.formData();
-        const question = (await data).get('question');
-        console.log("question: ", question)
+
+        if (!data) {
+            return fail(400);
+        }
+
+        const question = (await data).get('question') as string;
+
+        if (!question) {
+            return fail(400, { question, missing: true });
+        }
+
+        const completion = await openai.chat.completions.create({
+            messages: [{ role: 'system', content: `Using less than 50 words and in the tone of a master storyteller, answer this prompt: what if ${question}?` }],
+            model: 'gpt-3.5-turbo',
+        });
+
+        if (!completion?.choices?.length) {
+            return fail(400);
+        }
+
+        const imageResponse = await openai.images.generate({
+            prompt: `what if ${question}?`,
+            n: 1,
+            size: "256x256"
+        });
+
+        console.log(imageResponse)
+
+        return {
+            success: true,
+            textResponse: completion.choices[0].message.content,
+            imageResponse: imageResponse.data,
+            question
+        }
     }
+
 } satisfies Actions;
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,49 @@
-<script>
-    
+<script lang="ts">
+    import { enhance } from '$app/forms';
+    import type { ActionData } from './$types';
+    export let form: ActionData;
+    let formLoading = false;
+    $: 
 </script>
 
-<h1>What If...?</h1>
-<form method="POST">
-    <label for="question">(you fill in the rest)</label>
-    <input name="question" type="question" required />
-    <button>Go!</button>
-</form>
+<section class="container">
+    <h1>What If...?</h1>
+    <form method="POST" use:enhance={() => {
+        formLoading = true;
+        return async({ update }) => {
+            formLoading = false;
+            update();
+        }
+    }}>
+        <input name="question" type="question" required placeholder="the moon was made of cheese" />?
+        <button>Go!</button>
+    </form>
+    
+    {#if formLoading}
+        <p>loading....</p>
+    {/if}
+    
+    {#if form?.success}
+        <article>
+            <h2>What if {form.question}?</h2>
+            {#if form.imageResponse}
+                {#each form.imageResponse as image, i}
+                    <img src={image.url} alt={`what if machine response #${i}`} />
+                {/each}
+            {/if}
+            <p>{form.textResponse}</p>
+            <p>DEBUG: response length = {form?.textResponse?.length}</p>
+            <p>DEBUG: word count = {form.textResponse?.split(" ").length}</p>
+        </article>
+    {/if}
+
+</section>
+
+<style>
+    .container {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
+</style>
