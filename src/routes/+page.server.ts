5108da9ba92874afd478295d04872725fd648003
Ryan Price <rsprice90@gmail.com>
2024-01-10 15:14:02 -0800

    fix: add path to cookies.set

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -38,7 +38,7 @@ export const load: PageServerLoad = async (e: RequestEvent) => {
     const sessionId = await createUser(poolClient);
 
     if (sessionId) {
-      /* @migration task: add path argument */ /* @migration task: add path argument */ e.cookies.set("X-What-If-Machine-Session-Id", sessionId);
+      /* @migration task: add path argument */ /* @migration task: add path argument */ e.cookies.set("X-What-If-Machine-Session-Id", sessionId, { path: '/' });
     }
 
     poolClient.release();
